f8a1ccd94f32119a2851fbaea11c02a97b991d56
Dennis <energizer557@gmail.com>
2024-04-13 15:02:18 +0300

    Java simple singleton pattern implementation

---
+++ Singleton.java
@@ -0,0 +1,25 @@
+public class Singleton {
+
+    // Instance Variables
+    private static Singleton instance;
+    private String title;
+
+    /**Constructor**/
+    public Singleton(){;}
+
+    /**Returning the instance of current object while keeps only one instance of it**/
+    public static Singleton getInstance(){
+
+        if(instance ==  null){
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+    /**Returning the title of instance if the instance exists otherwise throw exception*/
+    public String getInstanceTitle() throws ExceptionInInitializerError {
+        if(instance == null) throw new ExceptionInInitializerError();
+        return instance.title;
+    }
+
+}
